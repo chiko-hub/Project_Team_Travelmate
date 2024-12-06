c2ccb7a68205e8f7afb8ea932d5479a5e8bb6c95
chiwon <cwpark1013@gmail.com>
2024-12-06 14:54:09 +0900

    fix: admin 로그인 수정

--- src/main/resources/static/admin/script/admin.js
+++ src/main/resources/static/admin/script/admin.js
@@ -1,138 +1,21 @@
-function go_search( url ){
-    document.frm.action = url;
-    document.frm.submit();
-}
+function showContent(step) {
+    const results = document.getElementById("results");
+    let content = "";
 
+    switch (step) {
 
-function go_detail( pseq ){
-    location.href="adminPlaceDetail?pseq=" + pseq;
-}
+        case "step1":
+            content = `
+        <h2>STEP 1: 장소 선택</h2>
+      `;
+            break;
+        case "step2":
+            content = `
+        <h2>STEP 2: 숙소 선택</h2>
+      `;
+            break;
 
-
-function go_write(){
-    location.href="adminPlaceWriteForm";
-}
-
-
-function cals(){
-    var value1 = document.placeWriteFrm.price1.value;
-    var value2 = document.placeWriteFrm.price2.value
-
-    if( value1=="" || value2==""){
-        return;
-    }else{
-        document.placeWriteFrm.price3.value = value2 - value1;
-    }
-}
-
-
-
-function go_save(){
-    var theForm = document.placeWriteFrm;
-    if( theForm.category.value==""){
-        alert('상품분류를 선택하세요');
-        theForm.kind.focus();
-    }else if (theForm.name.value == "") {
-        alert('상품명을 입력하세요.'); 				theForm.name.focus();
-    } else if (theForm.price1.value == "") {
-        alert('원가를 입력하세요.'); 				theForm.price1.focus();
-    } else if (theForm.price2.value == "") {
-        alert('판매가를 입력하세요.'); 			theForm.price2.focus();
-    } else if (theForm.content.value == "") {
-        alert('상품상세를 입력하세요.'); 		theForm.content.focus();
-    } else if (theForm.image.value == "") {
-        alert('상품이미지들 입력하세요.');		theForm.image.focus();
-    } else{
-        theForm.action = "adminPlaceWrite";
-        theForm.submit();
-    }
-}
-
-
-
-function go_mod(pseq){
-    location.href="adminPlaceUpdateForm?pseq=" + pseq;
-}
-
-
-
-function go_update(){
-    var theForm = document.placeWriteFrm;
-    if( theForm.category.value==""){
-        alert('상품분류를 선택하세요');
-        theForm.category.focus();
-    }else if (theForm.name.value == "") {
-        alert('상품명을 입력하세요.'); 				theForm.name.focus();
-    } else if (theForm.price1.value == "") {
-        alert('원가를 입력하세요.'); 				theForm.price1.focus();
-    } else if (theForm.price2.value == "") {
-        alert('판매가를 입력하세요.'); 			theForm.price2.focus();
-    } else if (theForm.content.value == "") {
-        alert('상품상세를 입력하세요.'); 		theForm.content.focus();
-    } else{
-        theForm.action = "adminPlaceUpdate";
-        theForm.submit();
     }
 
+    results.innerHTML = content;
 }
-
-
-
-function deletePlace(pseq){
-    var ans = confirm("정말로 삭제할까요?");
-    if( ans ){
-        location.href="deletePlace?pseq=" + pseq;
-    }
-}
-
-
-
-
-
-function go_order_next(){
-    var count=0;
-    if( document.frm.result.length == undefined ){
-        if( document.frm.result.checked == true ) count++;
-    }else{
-        for( var i=0; i<document.frm.result.length ; i++)
-            if( document.frm.result[i].checked==true)
-                count++;
-    }
-    if (count == 0) {
-        alert("주문처리할 항목을 선택해 주세요.");
-    }else{
-        document.frm.action = "adminOrderNext";
-        document.frm.submit();
-    }
-
-}
-
-
-
-
-
-function go_rep(){
-    if( document.frm.reply.value==""){
-        alert("답변내용을 입력하세요");
-        document.frm.reply.focus();
-    }else{
-        document.frm.action = "adminQnaReplyUpdate";
-        document.frm.submit();
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
