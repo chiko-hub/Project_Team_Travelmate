c8227433b70d5a1907571a37cb791f26e9e94790
hana <o3o_s2__@naver.com>
2024-12-11 19:49:57 +0900

    feat: wishlist 생성

--- src/main/java/com/himedia/tp01/service/WishlistService.java
+++ src/main/java/com/himedia/tp01/service/WishlistService.java
@@ -1,12 +1,25 @@
 package com.himedia.tp01.service;
 
 import com.himedia.tp01.dao.IWishlistDao;
+import com.himedia.tp01.dto.WishlistVO;
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WishlistService {
     @Autowired
     IWishlistDao wdao;
 
+    /* wishlist 조회 */
+    public List<WishlistVO> getWishlist(String userid) {
+        return wdao.getWishlist(userid);
+    }
+
+    /* wishlist 추가 */
+    public void addWishlist(WishlistVO wishlistvo) {
+        wdao.addWishlist(wishlistvo);
+    }
 }
